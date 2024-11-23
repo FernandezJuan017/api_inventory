024dfd62560c85cecfbcadbf22a651bfa9c623a7
Juan Cruz Fernandez <juancruzf017@gmail.com>
2024-11-23 11:29:44 -0300

    feat: create models

--- inventory/inventory_app/models.py
+++ inventory/inventory_app/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
-# Create your models here.
+# Brands
+class Brand(models.Model):
+    class Meta:
+        verbose_prural_name = 'Brands'
+
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
+# Category
+class Category(models.Model):
+    class Meta:
+        verbose_prural_name = 'Categories'
+
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
+# Warehouse
+class Warehouse(models.Model):
+    class Meta:
+        verbose_prural_name = 'Warehouses'
+
+    name = models.CharField(max_length=100)    
+    description = models.CharField(max_length=200)
+
+    def __str__(self):    
+        return self.name
+    
+# Product
+class Product(models.Model):
+    code = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.code} - {self.description}"
+    
+# InventoryProduct
+class InventoryProduct(models.Model):
+    class Meta:
+        verbose_prural_name = 'InventoryProducts'
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    warehouse = models.ForeignKey(Warehouse, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0) 
+    
+    def __str__(self):
+        return f"{self.quantity} - {self.product}  ({self.warehouse})"  
